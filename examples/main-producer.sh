e9cd06e5aad9987e00b3eee3254fb5dbbaed8e52
Jason Ribble <jason@companiontechnology.net>
2022-11-15 11:46:58 -0700

    :feat: customize scripts

--- examples/main-producer.sh
+++ examples/main-producer.sh
@@ -3,13 +3,13 @@
 docker network create -d bridge cardano
 docker run -it --rm \
     --network=cardano \
-    --name main-producing \
+    --name producer \
     -p 3000:3000 \
     -p 12798:12798 \
     -e HOST_ADDR="0.0.0.0" \
     -e NODE_PORT="3000" \
     -e NODE_NAME="block-producing" \
-    -e NODE_TOPOLOGY="<IP-address of relay1 node>:3000/1" \
+    -e NODE_TOPOLOGY="127.0.0.1:3000/1" \
     -e CARDANO_NETWORK="main" \
     -e PROMETHEUS_PORT="12798" \
     -v $PWD/config/:/config/ \
